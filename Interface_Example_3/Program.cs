3b955aa4a1ba520d153e1f0db47491debe1b90b5
Salman Asad <salmanasad9988@gmail.com>
2023-10-14 09:35:21 +0500

    Add project files.

---
+++ Interface_Example_3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Interface for walkable creatures
+interface IWalkable
+{
+    void Walk();
+}
+
+// Interface for flying creatures
+interface IFlyable
+{
+    void Fly();
+}
+
+// Class representing an animal that can walk
+class Animal : IWalkable
+{
+    public void Walk()
+    {
+        Console.WriteLine("The animal is walking.");
+    }
+}
+
+// Class representing an animal that can walk and fly
+class Bird : IWalkable, IFlyable
+{
+    public void Walk()
+    {
+        Console.WriteLine("The bird is walking.");
+    }
+
+    public void Fly()
+    {
+        Console.WriteLine("The bird is flying.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        Bird myBird = new Bird();
+
+        myAnimal.Walk();
+        myBird.Walk();
+        myBird.Fly();
+    }
+}
